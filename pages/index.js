9d1694f1f77d69a4f1cca7c0fdc0a2ec1934d89c
v3nom <rout.rudraprasad@gmail.com>
2023-06-22 16:38:39 +0530

    added posts

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,42 @@
 import Image from "next/image";
 import Head from "next/head";
+import{PostCard,Categories,PostWidget} from '../components'
+import {getPosts} from '../services'
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-];
+// const posts = [
+//   { title: "React Testing", excerpt: "Learn React Testing" },
+//   { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
+// ];
 
-export default function Home() {
+export default function Home({posts}) {
   return (
     <div className="container mx-auto px-10 mb-8 ">
       <Head>
         <title>Blog v2</title>
       </Head>
-      <div className="grid grid-cols-1 lg:grid-cold-12 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map((post, index) => (<PostCard post={post.node} key={post.title}/>))}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-              
+              <PostWidget/>
+              <Categories/>
             </div>
         </div>
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts())||[];
+  return {
+    props: {
+      posts,
+    },
+  };
 }
